b6d149f820525c817e57b270b49d2a319ed90d26
ArjDev69 <bruno96araujo@gmail.com>
2020-06-28 17:09:34 -0300

    checking platform

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
+import { Platform } from "react-native";
 
 const api = axios.create({
-  baseURL: "http://192.168.15.7:3333",
+  baseURL:
+    Platform.OS === "ios"
+      ? "http://localhost:3333"
+      : "http://192.168.15.7:3333",
 });
 
 export default api;
