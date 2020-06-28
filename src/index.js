d34383885dbcae516ff46669952ba86a3bf653fb
ArjDev69 <bruno96araujo@gmail.com>
2020-06-28 16:28:29 -0300

    setup store and api

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from "react";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
 import { StatusBar } from "react-native";
 
 import "./config/ReactotronConfig";
 
-import Routes from "~/routes";
+import { store, persistor } from "./store";
+import Routes from "./routes";
 
-const App = () => {
+export default function App() {
   return (
-    <>
-      <StatusBar barStyle="light-content" backgroundColor="#058996" />
-      <Routes />
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar barStyle="light-content" backgroundColor="#058996" />
+        <Routes />
+      </PersistGate>
+    </Provider>
   );
-};
-
-export default App;
+}
