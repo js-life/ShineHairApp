0e6c630ff6c09be66a5ab4e67434bd8ff40fe0d2
ArjDev69 <bruno96araujo@gmail.com>
2020-08-30 15:32:24 -0300

    schedules routes

---
+++ src/pages/New/SelectProvider/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { TouchableOpacity } from "react-native";
+
+import Icon from "react-native-vector-icons/MaterialIcons";
+
+import Background from "~/components/Background";
+
+const SelectProvider = () => {
+  return <Background />;
+};
+
+export default SelectProvider;
+
+SelectProvider.navigationOptions = ({ navigation }) => ({
+  title: "Selecione o prestador",
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate("Dashboard");
+      }}
+    >
+      <Icon name="chevron-left" size={20} color="#FFF" />
+    </TouchableOpacity>
+  ),
+});
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,16 @@
+import React from "react";
+
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
+
+import { createStackNavigator } from "react-navigation-stack";
+
 import { createBottomTabNavigator } from "react-navigation-tabs";
 
+import Icon from "react-native-vector-icons/MaterialIcons";
+
+import SelectDateTime from "./pages/New/SelectDateTime";
+import SelectProvider from "./pages/New/SelectProvider";
+import Confirm from "./pages/New/Confirm";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 
@@ -18,6 +28,35 @@ export default (isSigned = false) =>
         App: createBottomTabNavigator(
           {
             Dashboard,
+            New: {
+              screen: createStackNavigator(
+                {
+                  SelectProvider,
+                  SelectDateTime,
+                  Confirm,
+                },
+                {
+                  defaultNavigationOptions: {
+                    headerTransparent: true,
+                    headerTintColor: "#FFF",
+                    headerLeftContainerStyle: {
+                      marginLeft: 20,
+                    },
+                  },
+                }
+              ),
+              navigationOptions: {
+                tabBarVisible: false,
+                tabBarLabel: "Agendar",
+                tabBarIcon: (
+                  <Icon
+                    name="add-circle-outline"
+                    size={20}
+                    color="rgba(255, 255, 255, 0.6)"
+                  />
+                ),
+              },
+            },
             Profile,
           },
           {
