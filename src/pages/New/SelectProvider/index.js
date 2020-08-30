6e332e109c6388e6e9753f2a7d6973ccfd3e9b9d
ArjDev69 <bruno96araujo@gmail.com>
2020-08-30 16:29:29 -0300

    list provider

--- src/pages/New/SelectProvider/index.js
+++ src/pages/New/SelectProvider/index.js
@@ -1,13 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { TouchableOpacity } from "react-native";
 
 import Icon from "react-native-vector-icons/MaterialIcons";
 
+import api from "~/services/api";
+
 import Background from "~/components/Background";
 
-const SelectProvider = () => {
-  return <Background />;
+import { Container, ProvidersList, Provider, Avatar, Name } from "./styles";
+
+const SelectProvider = ({ navigation }) => {
+  const [providers, setProviders] = useState([]);
+
+  useEffect(() => {
+    async function loadProviders() {
+      const response = await api.get("providers");
+
+      setProviders(response.data);
+    }
+
+    loadProviders();
+  }, []);
+
+  return (
+    <Background>
+      <Container>
+        <ProvidersList
+          data={providers}
+          keyExtractor={(provider) => String(provider.id)}
+          renderItem={({ item: provider }) => (
+            <Provider
+              onPress={() =>
+                navigation.navigate("SelectDateTime", { provider })
+              }
+            >
+              <Avatar
+                source={{
+                  uri: provider.avatar
+                    ? provider.avatar.url
+                    : "https://avatars2.githubusercontent.com/u/6216736?s=460&u=163fdfb238cfcc0bffc8a53b2403929ff61743ba&v=4",
+                }}
+              />
+              <Name>{provider.name}</Name>
+            </Provider>
+          )}
+        />
+      </Container>
+    </Background>
+  );
 };
 
 export default SelectProvider;
